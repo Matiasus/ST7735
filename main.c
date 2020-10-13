456eeeb3a40241eecffbdfb44126044daaedd6be
Marián <Matiasus@users.noreply.github.com>
2020-10-13 10:42:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 /** 
- * Example of TFT controller st7735 / 1.8 TFT DISPLAY /
- *
- * Copyright (C) 2016 Marian Hrinko.
- * Written by Marian Hrinko (mato.hrinko@gmail.com)
+ * --------------------------------------------------------------------------------------------+ 
+ * @desc        Main example st7735 LCD driver
+ * --------------------------------------------------------------------------------------------+ 
+ *              Copyright (C) 2020 Marian Hrinko.
+ *              Written by Marian Hrinko (mato.hrinko@gmail.com)
  *
  * @author      Marian Hrinko
- * @datum       16.12.2017
+ * @datum       13.10.2020
  * @file        main.c
  * @tested      AVR Atmega16
- * @inspiration http://www.displayfuture.com/Display/datasheet/controller/ST7735.pdf
+ *
+ * @depend      st7735.h
+ * --------------------------------------------------------------------------------------------+
+ * @inspir      http://www.displayfuture.com/Display/datasheet/controller/ST7735.pdf
  *              https://github.com/adafruit/Adafruit-ST7735-Library
  *              http://w8bh.net/avr/AvrTFT.pdf
- *
  */
 #include <stdio.h>
 #include "lib/st7735.h"
-
 
 /**
  * @desc    Main
@@ -30,18 +32,13 @@ int main(void)
   St7735Init();
   // clear screen
   ClearScreen(0x0000);
-
   // set position
   SetPosition(2, 2);
-
-  DrawString("Dated 08/14/06. One amplifier that the 2N5416 PNP transistors that he purchased were NPN. I have tested them and verified this. Two of the transistors are shown in the linked photo. They are identified by the letters ON surrounded by a circle, signifying they are products of ON Semiconductor.You can check for a transistor type as follows:One amplifier that the 2N5416 PNP transistors that he purchased were NPN. One amplifier that the 2N5416 PNP transistors that he purchased were NPN", 0xffff, X1);
-
-  // zobrazenie */
+  // Draw string
+  DrawString("Dated 08/14/06. One amplifier that the 2N5416 PNP transistors were NPN", 0xffff, X1);
+  // update screen
   UpdateScreen();
-  // ukoncenie */
+  
+  // return
   return 0;
 }
-
-
-
-
